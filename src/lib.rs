2fce96b9e419c1122d2789ec016f7478b79e50d7
Miquel van Smoorenburg <mike@langeraar.net>
2020-12-22 23:00:51 +0100

    v0.1.0-alpha.5

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/curlyconf/0.1.0-alpha.4")]
+#![doc(html_root_url = "https://docs.rs/curlyconf/0.1.0-alpha.5")]
 //! ## Curlyconf
 //!
 //! Curlyconf is a configuration file reader for the configuration
